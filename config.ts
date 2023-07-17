c54a3c3da5c1357fb8de9d28a0ba4dc5faa9c383
Bendik Solheim <hello@bendik.dev>
2023-07-17 23:53:54 +0200

    Parse config file with Zod

--- config.ts
+++ config.ts
@@ -1,18 +1,28 @@
 import fs from "fs";
+import { ZodError, z } from "zod";
 import path from "path";
-import { Job } from "./scheduler.server";
+import { fromZodError } from "zod-validation-error";
 
 const isDev = process.env.NODE_ENV === "development";
 
-type Healthcheck = {
-  url: string;
-  expression: string;
-};
+const service = z.object({
+  service: z.string(),
+  expression: z.string(),
+  url: z.string().url(),
+  okStatusCode: z.number().int().positive().lte(599),
+});
 
-export type Config = {
-  services: Array<Job>;
-  healthcheck?: Healthcheck;
-};
+const healthcheck = z.object({
+  url: z.string().url(),
+  expression: z.string(),
+});
+
+const schema = z.object({
+  services: z.array(service),
+  healthcheck: healthcheck.optional(),
+});
+
+export type Config = z.infer<typeof schema>; // {
 
 function getConfig(): Config {
   try {
@@ -20,10 +30,16 @@ function getConfig(): Config {
     const config = isDev
       ? path.join(cwd, "config", "config.json")
       : path.join("/config", "config.json");
-    const doc: Config = JSON.parse(fs.readFileSync(config, "utf-8")) as Config;
+    const json = JSON.parse(fs.readFileSync(config, "utf-8"));
+    const doc = schema.parse(json);
     return doc;
   } catch (e) {
-    console.error("Error parsing config file", e);
+    if (e instanceof ZodError) {
+      const readableError = fromZodError(e);
+      console.log(readableError);
+    } else {
+      console.log("Error parsing config file", e);
+    }
     process.exit(1);
   }
 }
