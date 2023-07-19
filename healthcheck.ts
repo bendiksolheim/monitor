3969c39ed4bd9d41ebae90f8d6a9539cbd3a1210
Bendik Solheim <hello@bendik.dev>
2023-07-19 13:51:31 +0200

    Use log.ts from healthcheck.ts

--- healthcheck.ts
+++ healthcheck.ts
@@ -1,6 +1,7 @@
 import { scheduleFunction } from "./scheduler.server.js";
 import { getConfig } from "./config.js";
 import { getLatestStatus } from "./app/db.server.js";
+import { log } from "./log.js";
 
 export function configureHealthcheck() {
   const config = getConfig();
@@ -8,16 +9,16 @@ export function configureHealthcheck() {
   if (config.healthcheck) {
     const url = config.healthcheck.url;
     const expression = config.healthcheck.expression;
-    console.log(`├─ Healthcheck activated (${expression})`);
+    log(`├─ Healthcheck activated (${expression})`);
     // Ping healthcheck every N minutes
     scheduleFunction(async () => {
       const latestStatus = await getLatestStatus();
       const everythingOk = latestStatus.every((e) => e.status === "OK");
       if (everythingOk) {
-        console.log("Everything OK, pinging healthcheck");
+        log("Everything OK, pinging healthcheck");
         fetch(url);
       } else {
-        console.log("Some service is down, postponing healthcheck ping");
+        log("Some service is down, postponing healthcheck ping");
       }
     }, expression);
   }
