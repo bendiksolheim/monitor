48a037904c89da967628c110c81de009f19b76f7
Bendik Solheim <hello@bendik.dev>
2024-02-29 21:30:03 +0100

    Improve line graph with axes, Y values and grid lines

--- app/sparkline.tsx
+++ app/sparkline.tsx
@@ -1,19 +1,25 @@
-import { last, max, range, zip } from "./util/arrays";
+import { max, range, zip } from "./util/arrays";
 
 type Props = {
-  values: Array<number | null>;
+  values: Array<number | undefined>;
 };
 
 export function Sparkline(props: Props): JSX.Element {
+  const svgWidth = 750;
+  const svgHeight = 180;
+  const graphWidth = 710;
+  const graphHeight = 140;
   const yValues = props.values;
-  const xValues = range(yValues.length);
-  const width = xValues.length;
-  const height = (max(yValues) ?? 0) / 0.75;
-  const pairs = zip(xValues, yValues);
+  const xValues = range(graphWidth, graphWidth / yValues.length);
+  const maxValue = max(yValues) ?? 0;
+  const pairs: Array<Point> = zip(xValues, yValues).map(([x, y]) => ({
+    x,
+    y: ((y ?? 0) / maxValue) * graphHeight,
+  }));
   const paths = pairs.reduce(
     (paths, value) => {
       const current = paths[paths.length - 1];
-      if (typeof current[current.length - 1][1] === typeof value[1]) {
+      if (typeof current[current.length - 1].y === typeof value.y) {
         current.push(value);
       } else {
         paths.push([value]);
@@ -22,105 +28,148 @@ export function Sparkline(props: Props): JSX.Element {
     },
     [[pairs[0]]]
   );
-  const typedPaths: Array<Path> = paths.map((path) => {
-    if (path[0][1] !== null) {
-      return {
-        key: "operational",
-        values: path.map((p) => [p[0], p[1] ?? 0]),
-      };
+  const svgPaths = paths.flatMap((path) => {
+    if (path[0].y !== undefined) {
+      const p = path.map((p) => ({ x: p.x, y: p.y ?? 0 }));
+      return <OperationalPath key={path[0].x} path={p} height={graphHeight} />;
     } else {
-      return {
-        key: "error",
-        from: path[0][0] - 1,
-        to: path[path.length - 1][0] + 1,
-      };
+      const from = path[0].x - 1;
+      const to = path[path.length - 1].x + 1;
+      return (
+        <ErrorPath key={path[0].x} from={from} to={to} height={graphHeight} />
+      );
     }
   });
 
-  const svgPaths = typedPaths.flatMap((path) => {
-    switch (path.key) {
-      case "operational":
-        return makeOperationalPath(path, height);
-      case "error":
-        return makeErrorPath(path, height);
-    }
-  });
-
-  const viewBox = `0 0 ${width} ${height}`;
+  const viewBox = `0 0 ${svgWidth} ${svgHeight}`;
   return (
-    <svg
-      height="180px"
-      width="750px"
-      viewBox={viewBox}
-      preserveAspectRatio="none"
-    >
-      {svgPaths.map((path) => (
-        <path
-          key={path.id}
-          d={path.path}
-          stroke={path.stroke}
-          fill={path.fill}
-          strokeWidth={1}
-          vectorEffect="non-scaling-stroke"
+    <svg height={`${svgHeight}px`} width={`${svgWidth}px`} viewBox={viewBox}>
+      <g x="0" y="0" transform="translate(40, 20)">
+        {svgPaths}
+        <YAxis height={graphHeight} />
+        <XAxis width={graphWidth} height={graphHeight} />
+        <GridLines
+          height={graphHeight}
+          width={graphWidth}
+          maxValue={maxValue}
         />
-      ))}
+      </g>
+      <YValues maxValue={maxValue} height={graphHeight} />
     </svg>
   );
 }
 
-function makeOperationalPath(
-  path: OperationalPath,
-  height: number
-): Array<SvgPath> {
-  const values = path.values.map(([x, y]) => [x, height - y]);
+function YValues({
+  maxValue,
+  height,
+}: {
+  maxValue: number;
+  height: number;
+}): JSX.Element {
+  let heights = range(height, height / 5)
+    .concat(height)
+    .reverse()
+    .map((height) => height + 15);
+  let values = range(maxValue, maxValue / 5)
+    .concat(maxValue)
+    .map((value) => Math.ceil(value));
+  let points = zip(heights, values);
+  return (
+    <>
+      {points.map((point) => (
+        <text
+          key={point[0]}
+          x="32"
+          y={`${point[0]}`}
+          dominantBaseline="hanging"
+          fontSize="0.8rem"
+          textAnchor="end"
+        >
+          {point[1]}
+        </text>
+      ))}
+    </>
+  );
+}
+
+function YAxis({ height }: { height: number }): JSX.Element {
+  return <path d={`M 0 0 L 0 ${height}`} stroke="black" strokeWidth={1} />;
+}
+
+function XAxis({
+  width,
+  height,
+}: {
+  width: number;
+  height: number;
+}): JSX.Element {
+  return (
+    <path
+      d={`M 0 ${height} L ${width} ${height}`}
+      stroke="black"
+      strokeWidth={1}
+    />
+  );
+}
+
+function GridLines({
+  height,
+  width,
+}: {
+  maxValue: number;
+  height: number;
+  width: number;
+}): JSX.Element {
+  let values = range(height, height / 5);
+  return (
+    <>
+      {values.map((value) => (
+        <path
+          key={value}
+          d={`M 0 ${value} L ${width} ${value}`}
+          strokeDasharray="3"
+          stroke="black"
+          strokeWidth="1"
+          strokeOpacity="0.3"
+        />
+      ))}
+    </>
+  );
+}
+
+function OperationalPath(props: {
+  path: Array<Point>;
+  height: number;
+}): JSX.Element {
+  const values = props.path.map((p) => [p.x, props.height - (p.y ?? 0)]);
   const pathStart = "M";
   const pathLine = values.map((value) => `${value[0]} ${value[1]}`).join(" L ");
-  return [
-    {
-      id: path.values[0][0] + "-fill",
-      path: `${pathStart} ${pathLine} L ${
-        values[values.length - 1][0]
-      } ${height} L ${values[0][0]} ${height} Z`,
-      fill: "lightgreen",
-      stroke: "transparent",
-    },
-    {
-      id: path.values[0][0] + "-line",
-      path: `${pathStart} ${pathLine}`,
-      fill: "transparent",
-      stroke: "green",
-    },
-  ];
+  const path = `${pathStart} ${pathLine} L ${values[values.length - 1][0]} ${
+    props.height
+  } L ${values[0][0]} ${props.height} Z`;
+  return (
+    <>
+      <path d={path} fill="lightgreen" />
+      <path
+        d={`${pathStart} ${pathLine}`}
+        stroke="green"
+        strokeWidth="1"
+        fill="transparent"
+      />
+    </>
+  );
 }
 
-function makeErrorPath(path: ErrorPath, height: number): Array<SvgPath> {
-  const { from, to } = path;
-  return [
-    {
-      id: String(from + 1),
-      path: `M ${from} 0 L ${from} ${height} L ${to} ${height} L ${to} 0 Z`,
-      stroke: "transparent",
-      fill: "pink",
-    },
-  ];
-}
-
-type SvgPath = {
-  id: string;
-  path: string;
-  stroke: string;
-  fill: string;
-};
-
-type OperationalPath = {
-  key: "operational";
-  values: Array<[number, number]>;
-};
-
-type ErrorPath = {
-  key: "error";
+function ErrorPath(props: {
   from: number;
   to: number;
-};
+  height: number;
+}): JSX.Element {
+  const path = `M ${props.from} 0 L ${props.from} ${props.height} L ${props.to} ${props.height} L ${props.to} 0 Z`;
+  return <path d={path} fill="pink" strokeWidth="1" />;
+}
 
-type Path = OperationalPath | ErrorPath;
+type Point = {
+  x: number;
+  y: number | undefined;
+};
