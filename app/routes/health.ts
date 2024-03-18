62969d852cb98a12f99f6cb3533271d7b0a689b1
Bendik Solheim <bendik.solheim@bekk.no>
2024-03-18 18:16:23 +0100

    Better health page with more information

--- app/routes/health.ts
+++ app/routes/health.ts
@@ -1,11 +1,31 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { prisma } from "../db.server";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { getLatestStatus } from "../db.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   try {
-    await prisma.$queryRaw`select 1`;
-    return new Response("OK");
+    const latestStatus = await getLatestStatus();
+    const operational = latestStatus.every((e) => e.status === "OK");
+    if (operational) {
+      return json(
+        {
+          operational,
+        },
+        200
+      );
+    } else {
+      return json(
+        {
+          statuses: latestStatus,
+        },
+        500
+      );
+    }
   } catch (e) {
-    throw "Connection to DB missing";
+    return json(
+      {
+        message: "Could not retrieve events from database",
+      },
+      500
+    );
   }
 };
