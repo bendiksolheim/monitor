f9761e58b6b000d7ac1fda2e80248e959b8148dc
Bendik Solheim <hello@bendik.dev>
2024-07-10 17:30:15 +0200

    Display status/uptime as "pills" for last 24 hours

--- app/components/service.tsx
+++ app/components/service.tsx
@@ -1,36 +1,48 @@
-import { last } from "~/util/arrays";
-import { Sparkline } from "./sparkline";
-import { Badge, Card } from "@mantine/core";
+import { Badge, Card, Group, Space } from "@mantine/core";
 import { type Event } from "~/events";
+import { UptimeIndicator } from "./uptime-indicator";
+import { mapValues } from "~/util/record";
 
 export type ServiceStatus = "ok" | "failing" | "unknown";
 
 type ServiceProps = {
   name: string;
-  events: Array<Event>;
+  events: Record<PropertyKey, Array<Event>>;
   status: ServiceStatus;
 };
 
 export function Service(props: ServiceProps): JSX.Element {
   const { name, events, status } = props;
+  const allEvents: Array<Event> = Object.values(events).flat();
+  const uptime = allEvents.filter((e) => e.ok).length / allEvents.length;
+  const uptimePercentage = maxTwoDecimals(uptime * 100);
   return (
-    <Card shadow="xs" withBorder p="md" style={{ minHeight: 227 }}>
-      <Badge variant="light" color={serviceStatusToColor(status)} size="md">
-        {name}
-      </Badge>
-      <div>
-        <Status events={events} />
-      </div>
+    <Card shadow="xs" withBorder p="md">
+      <Group justify="space-between">
+        <Badge variant="light" color={serviceStatusToColor(status)} size="md">
+          {name}
+        </Badge>
+        <div>{uptimePercentage}%</div>
+      </Group>
+      <Space h="lg" />
+      <Status events={events} name={name} />
     </Card>
   );
 }
 
-function Status(props: { events: Array<Event> }): JSX.Element {
-  const latencies = props.events.map((event) => event.latency);
-  if (props.events.length === 0) {
+function Status(props: {
+  events: Record<PropertyKey, Array<Event>>;
+  name: string;
+}): JSX.Element {
+  if (Object.keys(props.events).length === 0) {
     return <span>Ingen status enda</span>;
   } else {
-    return <Sparkline values={latencies} />;
+    const values = mapValues(props.events, (events) =>
+      events.map((event) => event.ok)
+    );
+    return <UptimeIndicator values={values} name={props.name} />;
+    /* const latencies = props.events.map((event) => event.latency); */
+    /* return <Sparkline values={latencies} />; */
   }
 }
 
@@ -43,4 +55,15 @@ function serviceStatusToColor(status: ServiceStatus): string {
     case "unknown":
       return "yellow";
   }
+}
+
+/**
+ * Weird hack to round numbers to at most 2 decimals.
+ * 10 => 10
+ * 10.1 => 10.1
+ * 10.01 => 10.01
+ * 10.005 => 10.01
+ */
+function maxTwoDecimals(n: number): number {
+  return +n.toFixed(2);
 }
