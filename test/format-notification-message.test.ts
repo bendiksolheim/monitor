5334a243f83db35542331a266912df8ecd112d5f
Bendik Solheim <hello@bendik.dev>
2024-07-19 23:56:11 +0200

    Add services names to notification

--- test/format-notification-message.test.ts
+++ test/format-notification-message.test.ts
@@ -3,29 +3,37 @@ import { expect, test } from "vitest";
 import { formatNotificationMessage } from "../server/healthchecks/format-notification-message";
 import { range } from "~/util/arrays";
 
-test("single services down", () => {
-  const events = randomEvents(1);
-  const message = formatNotificationMessage(events);
-  expect(message).toBe("1 service down");
-});
-
-test("multiple services down", () => {
-  const events = randomEvents(2);
-  const message = formatNotificationMessage(events);
-  expect(message).toBe("2 services down");
-});
-
 test("no services down", () => {
   const events = randomEvents(0);
   const message = formatNotificationMessage(events);
   expect(message).toBeNull();
 });
 
+test("single service down", () => {
+  const events = randomEvents(1);
+  const message = formatNotificationMessage(events);
+  expect(message).toBe("1 service down: my-service-0");
+});
+
+test("two services down", () => {
+  const events = randomEvents(2);
+  const message = formatNotificationMessage(events);
+  expect(message).toBe("2 services down: my-service-0 and my-service-1");
+});
+
+test("three services down", () => {
+  const events = randomEvents(3);
+  const message = formatNotificationMessage(events);
+  expect(message).toBe(
+    "3 services down: my-service-0, my-service-1 and my-service-2"
+  );
+});
+
 function randomEvents(n: number): Array<Event> {
-  return range(n).map((_) => {
+  return range(n).map((m) => {
     return {
       id: 0,
-      service: "my-service",
+      service: `my-service-${m}`,
       ok: false,
       created: new Date(),
       latency: 100,
