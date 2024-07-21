5ce339b803562489b6cb666928ac3250bec936b1
Bendik Solheim <hello@bendik.dev>
2024-07-21 21:13:36 +0200

    Expose version in health endpoint

--- app/routes/health.ts
+++ app/routes/health.ts
@@ -2,12 +2,15 @@ import { json } from "@remix-run/node";
 import events from "~/events";
 
 export const loader = async () => {
+  //@ts-ignore env.* does not have typings
+  const version = import.meta.env.VITE_VERSION;
   try {
     const latestStatus = await events.latestStatus();
     const operational = latestStatus.every((e) => e.ok);
     if (operational) {
       return json(
         {
+          version: version,
           operational,
         },
         200
@@ -15,6 +18,7 @@ export const loader = async () => {
     } else {
       return json(
         {
+          version: version,
           statuses: latestStatus,
         },
         500
@@ -23,6 +27,7 @@ export const loader = async () => {
   } catch (e) {
     return json(
       {
+        version: version,
         message: "Could not retrieve events from database",
       },
       500
